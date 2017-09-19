8dbdb363940638a80e2a8c73a2b206b560838cbb
Anjack <gary.reif@gmail.com>
2017-09-19 13:02:07 -0400

    Improved tree generator and added random offset function.

--- MakeAScene.js
+++ MakeAScene.js
@@ -79,33 +79,68 @@ function redrawCanvas(canvas, ctx) {
  */
 function makeRect(ctx, x, y, w, h, fill) {
 	if (fill != null) {
-		ctx.fillStyle = fill
+		ctx.fillStyle = "#BF7028"
 		ctx.fillRect(x, y, w, h)
+		ctx.strokeStyle = "#8A511D"
+		ctx.strokeRect(x, y, w, h)
 	}
 	else {
 		ctx.strokeRect(x, y, w, h)
 	}
 }
 
+/**
+ * Generates a tree of specified height.
+ *
+ * @param {object} ctx
+ * @param {integer} x
+ * @param {integer} y
+ * @param {integer} tree_h
+ */
 function generateTree(ctx, x, y, tree_h) {
 	trunk_height = (tree_h / 7) * 6
-	trunk_width = trunk_height / 4
-	canopy_height = trunk_height / 3
-	canopy_width = 3 * trunk_width
+	trunk_width = randomOffset(Math.floor(trunk_height / 4), 50)
+	canopy_height = randomOffset(Math.floor(trunk_height / 3), 75)
+	canopy_width = randomOffset(Math.floor(2 * trunk_width), 20)
 
 	makeTree(ctx, x, y, trunk_width, trunk_height, canopy_width, canopy_height)
+}
+
+/**
+ * Generates a rectangle group of trees with specified average height.
+ * 
+ * @param {object} ctx
+ * @param {integer} x
+ * @param {integer} y
+ * @param {integer} w
+ * @param {integer} h
+ * @param {integer} avg_h
+ */
+function generateTreeStand(ctx, x, y, w, h, avg_h) {
+
+}
+
+/**
+ * Returns an integer within the specified percentage of supplied number.
+ * @param {integer} num		Base number for calculation.
+ * @param {integer} offset	Max percentage offset allowed from base.
+ */
+function randomOffset(num, offset) {
+	change_amt = Math.floor(Math.random() * offset)
+	console.log(change_amt)
+	return num + ((num / 100) * change_amt)
 }
 
 /**
  * Draws a simple tree-like shape. Will have a brown trunk and green canopy.
  *
  * @param {object} ctx
- * @param {int} x			X-coordinate of bottom center of tree (trunk).
- * @param {int} y			Y-coordinate of bottom center of tree (trunk).
- * @param {int} trunk_w		Width of the trunk rectangle.
- * @param {int} trunk_h		Height of the trunk rectangle.
- * @param {int} canopy_w	Width of the ellipse at the top of the tree.
- * @param {int} canopy_h	Height of the ellipse at the top of the tree.
+ * @param {integer} x			X-coordinate of bottom center of tree (trunk).
+ * @param {integer} y			Y-coordinate of bottom center of tree (trunk).
+ * @param {integer} trunk_w		Width of the trunk rectangle.
+ * @param {integer} trunk_h		Height of the trunk rectangle.
+ * @param {integer} canopy_w	Width of the ellipse at the top of the tree.
+ * @param {integer} canopy_h	Height of the ellipse at the top of the tree.
  */
 function makeTree(ctx, x, y, trunk_w, trunk_h, canopy_w, canopy_h) {
 	trunk_x = x - (trunk_w / 2)
@@ -117,6 +152,8 @@ function makeTree(ctx, x, y, trunk_w, trunk_h, canopy_w, canopy_h) {
 	ctx.ellipse(canopy_x, canopy_y, canopy_w, canopy_h, 0, 0, 2 * Math.PI)
 	ctx.fillStyle = "#0CE868"
 	ctx.fill()
+	ctx.strokeStyle = "#09AD4E"
+	ctx.stroke()
 	ctx.closePath()
 }
 
